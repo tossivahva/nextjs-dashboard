d123506a9d35cef3f8562da3fac0c6db82a66a39
tossivahva <tossivahva@gmail.com>
2023-11-22 21:23:12 +0300

    Authentication and autharization with NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,7 @@ import {sql} from '@vercel/postgres';
 import {revalidatePath} from 'next/cache';
 import {redirect} from 'next/navigation';
 import exp from 'constants';
+import {signIn} from '@/auth';
 
 const FormSchema = z.object({
    id: z.string(),
@@ -114,3 +115,18 @@ export async function deleteInvoice(id: string) {
       };
    }
 };
+
+//Sign In
+export async function authenticate(
+   prevState: string | undefined,
+   formData: FormData,
+) {
+   try {
+      await signIn('credentials', Object.fromEntries(formData));
+   } catch (error) {
+      if ((error as Error).message.includes('CredentialsSignin')) {
+         return 'CredentialsSignin';
+      }
+      throw error;
+   }
+}
